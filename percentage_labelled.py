b3e437b58211813f916ff1be2cfe247d6908f48e
dieuwkehupkes <dieuwkehupkes@gmail.com>
2013-10-15 11:16:31 +0200

    compute how many spans are labelled by an SAMT label

---
+++ percentage_labelled.py
@@ -0,0 +1,14 @@
+import sys
+from file_processing import *
+
+alignments = sys.argv[1]
+sentences = sys.argv[2]
+trees = sys.argv[3]
+
+f = ProcessDependencies(alignments, sentences, trees)
+l = f.labelled_SAMT(40)
+print 'total: %i\n' % l[0]
+print 'labelled SAMT: %i\n' % l[1]
+percentage = float(l[1])/l[0]
+print 'average labelled: %f' % percentage
+#print 'average labelled: %f' % float(l[1])/float(l[0])
