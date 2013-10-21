ca2de324b46527bde9406d002db0cb30e2b1f24c
dieuwkehupkes <dieuwkehupkes@gmail.com>
2013-10-21 12:04:46 +0200

    Included function for new HATs class

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ from tests_scoring import *
 from tests_alignments import *
 from tests_rule import *
 from tests_labelling import *
+from tests_HATforest import *
 
 #create testing objects for all classes and print outcome
 
@@ -15,12 +16,14 @@ class Tests():
 		a = AlignmentsTests()
 		r = RuleTests()
 		l = LabelsTests()
+		h = HATsTests()
 		print '\nDependency class functions as intended:\t', d.dependencies_test_all()
 		print 'Node class functions as intended:\t', n.path_test_all()
 		print 'Scoring class functions as intended:\t', s.score_test_all()
 		print 'Rules class functions as intended:\t', r.rules_test_all()
 		print 'Alignments class functions as intended:\t', a.spans_test_all()
-		print  'Label class functions as intenced:\t', l.test_all(), '\n'
+		print  'Label class functions as intenced:\t', l.test_all()
+		print  'HATs class functions as intenced:\t', h.test_all(), '\n'
 	
 
 if __name__ == "__main__":
